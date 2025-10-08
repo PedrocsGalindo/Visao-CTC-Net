84b7f4dd0140e2bcaf5555ad62fe76165e256508
Pedro Cesar Galindo <pedrocsgalindo@gmail.com>
2025-10-07 21:18:26 -0300

    feat/load_data

--- settings.py
+++ settings.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
+from pathlib import Path
 import os
 
 load_dotenv()  
 TORCH_CACHE_DIR = os.getenv("TORCH_CACHE_DIR")
 SYS_TOKEN = os.getenv("SYS_TOKEN")
+
+BASE = Path(__file__).parent.resolve()
