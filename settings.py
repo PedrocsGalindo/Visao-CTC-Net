7b1b3889f452dc850c3f0ed49cb008cf293b85b1
Pedro Cesar Galindo <pedrocsgalindo@gmail.com>
2025-10-22 13:55:33 -0300

    refact

--- settings.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-from pathlib import Path
-import os
-
-load_dotenv()  
-TORCH_CACHE_DIR = os.getenv("TORCH_CACHE_DIR")
-SYS_TOKEN = os.getenv("SYS_TOKEN")
-
-BASE = Path(__file__).parent.resolve()
